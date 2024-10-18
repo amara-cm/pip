176808a236c0ba2faa070d062ddbcb2ba00b664c
Angel <cicelyyui@gmail.com>
2024-10-18 17:56:02 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,11 @@ import webpack from 'webpack';
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+const require = createRequire(import.meta.url);
 
 // Workaround for __filename and __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const require = createRequire(import.meta.url); // Allows usage of require in ESM
 
 const nextConfig = {
   webpack: (config) => {
