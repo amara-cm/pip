91fa4fe9a9a211e15368f83db2598f44467436df
Angel <cicelyyui@gmail.com>
2024-10-18 09:29:03 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import '../global.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-=======
-import '../styles/globals.css';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
->>>>>>> f0ebc03e658ddc2c06dfc298591f21089614fc6b
