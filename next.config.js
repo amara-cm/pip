5d5edd4186d2603f7a6dcdf9780645baa40b0d18
Angel <cicelyyui@gmail.com>
2024-10-20 23:21:32 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,29 +2,31 @@ import webpack from 'webpack';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-// Workaround for __filename and __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      https: false,
-      http: false,
-      net: false,
-      buffer: 'buffer',  // This is a string instead of using require
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        https: false,
+        http: false,
+        net: false,
+        buffer: false, // Change this to false
+        stream: false, // Add this
+        url: false, // Add this
+      };
+    }
 
     config.plugins.push(
       new webpack.ProvidePlugin({
         Buffer: ['buffer', 'Buffer'],
-      })
+      }),
     );
 
-    // Adjust Webpack caching to avoid cache serialization issues
     config.cache = {
-      type: 'memory',  // Keep this as it is
+      type: 'memory',
     };
 
     return config;
