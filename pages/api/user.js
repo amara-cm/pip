0f256cd537a94eb54c5acd62a1f3abe51ce13076
Angel <cicelyyui@gmail.com>
2024-10-22 14:59:11 +0800

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,33 +1,30 @@
 import prisma from '../../lib/db';
 
 export default async function handler(req, res) {
-  // Check for the request method
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
-  const { telegramUsername } = req.body; // Expecting telegramUsername in the request body
+  const { telegramUsername } = req.body;
 
-  // Validate telegramUsername
   if (!telegramUsername) {
     return res.status(400).json({ error: 'Telegram username is required' });
   }
 
   try {
     const user = await prisma.user.findUnique({
-      where: { telegramUsername: telegramUsername }, // Fetch the user by telegramUsername
+      where: { telegramUsername },
     });
 
-    // Check if the user exists
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    res.status(200).json(user); // Respond with user data
+    res.status(200).json(user);
   } catch (error) {
-    console.error('Error fetching user:', error); // Log error for debugging
+    console.error('Error fetching user:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   } finally {
-    await prisma.$disconnect(); // Ensure Prisma Client disconnects after the request
+    await prisma.$disconnect();
   }
 }
