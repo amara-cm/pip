2cc272a7d89c9096fcec6b46cbbba2c418922e59
Angel <cicelyyui@gmail.com>
2024-10-22 14:49:35 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // We're in the browser build, exclude node built-in modules
       config.resolve.fallback = {
         fs: false,
         https: false,
@@ -9,6 +10,9 @@ const nextConfig = {
         buffer: false,
         stream: false,
         url: false,
+        crypto: false, // Add any others if needed
+        tls: false,
+        child_process: false,
       };
     }
 
