e9ee4e98bb9f5790f0c49de0b9f88d36a5ff3f40
Angel <cicelyyui@gmail.com>
2024-10-22 20:57:43 +0800

    Update friends.js

--- pages/friends.js
+++ pages/friends.js
@@ -1,38 +1,38 @@
 import { useState } from 'react';
+import styles from '../styles/Friends.module.css';
 
 const Friends = () => {
   const [referrals, setReferrals] = useState([]);
   const [newReferralId, setNewReferralId] = useState('');
 
   const handleInvite = () => {
-    // Mock invite logic
     setReferrals([...referrals, newReferralId]);
     setNewReferralId('');
   };
 
   return (
-    <div className="friends-scr">
-      <div className="frame">
-        <div className="inv-text">Invite Friends</div>
-        <div className="frame2">
+    <div className={styles['friends-scr']}>
+      <div className={styles.frame}>
+        <div className={styles['inv-text']}>Invite Friends</div>
+        <div className={styles.frame2}>
           <input
             type="text"
             placeholder="Enter referral ID"
             value={newReferralId}
             onChange={(e) => setNewReferralId(e.target.value)}
           />
-          <button className="send-btn" onClick={handleInvite}>Send</button>
+          <button className={styles['send-btn']} onClick={handleInvite}>Send</button>
         </div>
       </div>
-      <div className="referrals-list">
+      <div className={styles['referrals-list']}>
         {referrals.map((ref, index) => (
-          <div key={index} className="referral">{ref}</div>
+          <div key={index} className={styles.referral}>{ref}</div>
         ))}
       </div>
-      <div className="tab-bar">
-        <button className="tab-btn tab-rewards" onClick={() => window.location.href = '/tasks'}>Rewards</button>
-        <button className="tab-btn tab-site" onClick={() => window.location.href = '/home'}>Site</button>
-        <button className="tab-btn tab-friends active" onClick={() => window.location.href = '/friends'}>Friends</button>
+      <div className={styles['tab-bar']}>
+        <button className={styles['tab-btn']} onClick={() => window.location.href = '/tasks'}>Rewards</button>
+        <button className={styles['tab-btn']} onClick={() => window.location.href = '/home'}>Site</button>
+        <button className={`${styles['tab-btn']} active`} onClick={() => window.location.href = '/friends'}>Friends</button>
       </div>
     </div>
   );
