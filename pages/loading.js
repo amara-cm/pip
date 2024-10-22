ebb0de32bb28c5f35d31d3440f042137e927a450
Angel <cicelyyui@gmail.com>
2024-10-22 16:10:03 +0800

    Update loading.js

--- pages/loading.js
+++ pages/loading.js
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const Loading = () => {
   const [progress, setProgress] = useState(0);
+  const router = useRouter();
 
   useEffect(() => {
     const interval = setInterval(() => {
       setProgress(prev => {
         if (prev >= 100) {
           clearInterval(interval);
-          window.location.href = '/home';  // Redirect to Home page when loading is complete
+          router.push('/home');  // Redirect to Home page when loading is complete
         }
         return prev + 1;
       });
     }, 100); // Adjust the time as needed
     return () => clearInterval(interval);
-  }, []);
+  }, [router]);
 
   return (
     <div className="loading-scr">
