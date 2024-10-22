dac4fed339c310ee9493e469dd499df1f97a1325
Angel <cicelyyui@gmail.com>
2024-10-22 22:10:13 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,4 @@
 import '../styles/global.css';
-import styles from '../styles/Loading.module.css';
-import styles from '../styles/Home.module.css';
-import styles from '../styles/Tasks.module.css';
-import styles from '../styles/Friends.module.css';
-import styles from '../styles/Error.module.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
