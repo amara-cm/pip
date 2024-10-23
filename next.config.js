2624dc22d8a1c790b98cfaf14dfb129c4a65dca1
amara-cm <cicelyyui@gmail.com>
2024-10-23 23:37:21 +0800

    d

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
 import BuilderDevTools from "@builder.io/dev-tools/next";
 
-const nextConfig = BuilderDevTools()({
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        https: false,
-        http: false,
-        net: false,
-        buffer: false,
-        stream: false,
-        url: false,
-        crypto: false,
-        tls: false,
-        child_process: false,
-      };
-    }
-    return config;
-  },
-});
+const nextConfig = BuilderDevTools()(
+  BuilderDevTools()({
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false,
+          https: false,
+          http: false,
+          net: false,
+          buffer: false,
+          stream: false,
+          url: false,
+          crypto: false,
+          tls: false,
+          child_process: false,
+        };
+      }
+      return config;
+    },
+  })
+);
 
 export default nextConfig;
