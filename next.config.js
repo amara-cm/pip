fdba8f2451c822abf2795b9bfdc8cea2df0f93dc
Angel <cicelyyui@gmail.com>
2024-10-24 14:10:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,21 @@
-import BuilderDevTools from "@builder.io/dev-tools/next";
-
-const nextConfig = BuilderDevTools()(
-  BuilderDevTools()({
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-          https: false,
-          http: false,
-          net: false,
-          buffer: false,
-          stream: false,
-          url: false,
-          crypto: false,
-          tls: false,
-          child_process: false,
-        };
-      }
-      return config;
-    },
-  })
-);
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        https: false,
+        http: false,
+        net: false,
+        buffer: false,
+        stream: false,
+        url: false,
+        crypto: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
