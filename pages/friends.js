a22e940ea61d089ca39d273c625ba4173a27fcfa
Angel <cicelyyui@gmail.com>
2024-10-28 00:20:15 +0800

    Update friends.js

--- pages/friends.js
+++ pages/friends.js
@@ -43,19 +43,19 @@ const Friends = () => {
       </div>
 
       <footer className="flex justify-around p-4 bg-black fixed bottom-0 w-full max-w-[30rem] gap-[0.4375rem]">
-        <button onClick={() => window.location.href = '/tasks'} 
+        <button onClick={() => window.location.href = '/tasks'}>
           <div style={{ pointerEvents: 'none' }}>
             <img src="/icons/arewards.svg" alt="Rewards" />
           </div>
         </button>
-        <button onClick={() => window.location.href = '/'} 
+        <button onClick={() => window.location.href = '/'}>
           <div style={{ pointerEvents: 'none' }}>
             <img src="/icons/bsite.svg" alt="Home" />
           </div>
         </button>
-        <button className="cursor-not-allowed" 
+        <button className="cursor-not-allowed">
           <div style={{ pointerEvents: 'none' }}>
-           <img src="/icons/bfriends.svg" alt="Friends" />
+            <img src="/icons/afriends.svg" alt="Friends" />
           </div>
         </button>
       </footer>
