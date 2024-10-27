02e45491cf4f92ec61ee64e68babd60395115310
Angel <cicelyyui@gmail.com>
2024-10-27 16:27:54 +0800

    Update error.js

--- pages/error.js
+++ pages/error.js
@@ -1,14 +1,14 @@
-import styles from '../styles/Error.module.css';
-
 const Error = () => {
   const handleRefresh = () => {
     window.location.reload();
   };
 
   return (
-    <div className={styles['error-scr']}>
-      <div className={styles.text}>Try Again</div>
-      <button className={styles['refresh-btn']} onClick={handleRefresh}>Refresh</button>
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-black text-white font-outfit font-semibold">
+      <div className="text-2xl mb-4">Try Again</div>
+      <button onClick={handleRefresh} className="px-4 py-2 bg-red-500 rounded text-xl">
+        Refresh
+      </button>
     </div>
   );
 };
