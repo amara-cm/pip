e313dbecc861a480fb80eb2fbe836b48a8890bfc
Angel <cicelyyui@gmail.com>
2024-10-29 23:21:26 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -154,10 +154,6 @@ function HomeScreen() {
           </button>
         )}
       </div>
-      <div className="coins" style={{ marginTop: '25%', pointerEvents: 'none' }}>
-        <div className="coin-icon"></div>
-        <div className="coin-amt">{coins}</div>
-      </div>
       <footer className="flex justify-around p-4 bg-black fixed bottom-0 w-full max-w-[30rem] gap-[0.4375rem]">
         <button onClick={() => window.location.href = '/tasks'} className="flex justify-center items-center">
           <div style={{ pointerEvents: 'none' }}>
