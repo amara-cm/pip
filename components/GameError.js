d95528d31ad2ce0b56c6bf7fbed6baeeb1b225d2
Angel <cicelyyui@gmail.com>
2024-10-30 23:27:31 +0800

    Create GameError.js

---
+++ components/GameError.js
@@ -0,0 +1,29 @@
+// components/GameError.js
+
+import { useRouter } from 'next/router';
+
+const GameError = () => {
+  const router = useRouter();
+
+  const handleRefresh = () => {
+    router.reload(); // Reload the current page
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-black text-white font-outfit font-semibold">
+      <div className="text-20 mb-4">Try Again</div>
+      <button 
+        onClick={handleRefresh} 
+        className="flex justify-center items-center"
+      >
+        <img 
+          src="/icons/refresh.svg" 
+          alt="Refresh" 
+          style={{ pointerEvents: 'none' }} // Disable pointer events for the icon
+        />
+      </button>
+    </div>
+  );
+};
+
+export default GameError;
