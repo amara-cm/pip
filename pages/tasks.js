d45c6c0710c2f027b494962a35263c4770e6b9ab
Angel <cicelyyui@gmail.com>
2024-10-30 18:06:50 +0800

    Update tasks.js

--- pages/tasks.js
+++ pages/tasks.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Footer from './Footer';
+import Footer from '../components/Footer';
 
 // DailyLogin Component
 const DailyLogin = ({ currentDay, loginTimer, handleClaim }) => {
