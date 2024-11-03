bb691d5013ad74cfaa24a3eb851e9d99055d3b37
Angel <cicelyyui@gmail.com>
2024-11-04 06:30:02 +0800

    Delete utils directory

--- utils/uid.js
+++
@@ -1,8 +0,0 @@
-export function generateUID() {
-  const chars = '0123456789ABC';
-  let uid = '';
-  for (let i = 0; i < 7; i++) {
-    uid += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return uid;
-}
