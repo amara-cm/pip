096c7691e6519f5c44be59b4d55365a963626771
Angel <cicelyyui@gmail.com>
2024-11-04 11:55:01 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,11 @@ import Loading from '../components/loading'; // Import your Loading component
 import GameError from '../components/GameError'; // Import your GameError component
 
 function MyApp({ Component, pageProps }) {
-  const [isLoading, setIsLoading] = useState(true); // State to track loading
-  const [hasLoadingError, setHasLoadingError] = useState(false); // State to track loading errors
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasLoadingError, setHasLoadingError] = useState(false);
 
   useEffect(() => {
-    // This function will handle the asset preloading process
+    // Preload assets
     const preloadAssets = async () => {
       const assets = [
         '/icons/arewards.svg',
@@ -37,28 +37,28 @@ function MyApp({ Component, pageProps }) {
       };
 
       try {
-        await Promise.all(assets.map(preloadAsset)); // Preload assets
-        setIsLoading(false); // Set loading to false after assets are preloaded
+        await Promise.all(assets.map(preloadAsset));
+        setIsLoading(false);
       } catch (error) {
         console.error('Error preloading assets:', error);
-        setHasLoadingError(true); // Set loading error state
+        setHasLoadingError(true);
       }
     };
 
-    preloadAssets(); // Start the preloading process
+    preloadAssets();
   }, []);
 
-  // Render the loading screen while assets are preloading
+  // Render loading screen while assets are being preloaded
   if (isLoading) {
     return <Loading />;
   }
 
-  // Render the GameError component if there's a loading error
+  // Render GameError if there's an error during loading
   if (hasLoadingError) {
     return <GameError />;
   }
 
-  // Render the main app component if everything is loaded successfully
+  // Render the actual page component
   return <Component {...pageProps} />;
 }
 
