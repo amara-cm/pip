aab0b74061b109435545c8400a6997c5b7af5e57
Angel <cicelyyui@gmail.com>
2024-11-05 14:52:03 +0800

    Create telegram.js

---
+++ pages/api/telegram.js
@@ -0,0 +1,14 @@
+import prisma from '../../lib/db';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { id, username, first_name } = req.body;
+    await prisma.user.upsert({
+      where: { user_id: String(id) },
+      update: { username, first_name },
+      create: { user_id: String(id), username, first_name },
+    });
+    return res.status(201).json({ message: 'User created/updated' });
+  }
+  return res.status(405).json({ error: 'Method Not Allowed' });
+}
